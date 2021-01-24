8212e5ffc850d1b91269c3bffdc38d5056024eab
Aaron John Asuncion <aaronjohn.asuncion@gmail.com>
2021-01-24 17:01:51 +0800

    Initial Commit.

---
+++ src/main/java/com/aaron/recipewebspringboot/RecipeWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aaron.recipewebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeWebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeWebSpringbootApplication.class, args);
+	}
+
+}
